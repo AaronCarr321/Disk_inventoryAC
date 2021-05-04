9d18ed7b0694d0a561b8785b4fccb29a527864c9
AaronCarr321 <80131030+AaronCarr321@users.noreply.github.com>
2021-05-03 20:15:40 -0600

    project2
    This is project2 from 4-16-2021

---
+++ diskInv/diskInv/DiskInventory/DiskInventory/Controllers/BorrowerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiskInventory.Models;
+
+namespace DiskInventory.Controllers
+{
+    public class BorrowerController : Controller
+    {
+        private Disk_inventoryACContext context { get; set; }           // Student context name will differ
+        public BorrowerController(Disk_inventoryACContext ctx)            // Student context name will differ
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            List<Borrower> borrowers = context.Borrowers.OrderBy(a => a.Lname).ToList();
+            return View(borrowers);
+        }
+    }
+}
---
+++ diskInv/diskInv/DiskInventory/DiskInventory/Controllers/DiskController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiskInventory.Models;
+
+namespace DiskInventory.Controllers
+{
+    public class DiskController : Controller
+    {
+        private Disk_inventoryACContext context { get; set; }         
+        public DiskController(Disk_inventoryACContext ctx)           
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            List<Disk> disks = context.Disks.OrderBy(a => a.DiskName).ToList();
+            return View(disks);
+        }
+    }
+}
